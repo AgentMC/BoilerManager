c8e7dc1b1ef622ffbc908153d131fd7019410344
Mike Makarov <hackdesigner2010@hotmail.com>
2022-07-13 23:11:47 +0100

    Chart draws fetched data and auto-updates.

--- BoilerManager/Model/BoilerMeta.cs
+++ BoilerManager/Model/BoilerMeta.cs
@@ -4,21 +4,27 @@
     {
         const double FullWarm = 45.0, DefCold = 25;
 
-        public double BoilerWarm { get; init; }
-        public double BoilerTime { get; init; }
-        public DateTime LastNotified { get; init; }
+        public double BoilerWarm { get; private set; }
+        public double BoilerTime { get; private set; }
+        public DateTime LastNotified { get; private set; }
 
         public Dictionary<DateTime, double[]> Readings { get; init; }
 
         public BoilerMeta(Dictionary<DateTime, double[]> readings)
         {
             Readings = readings;
-            LastNotified = readings.Keys.Max();
+            Update();
+        }
+
+        public void Update()
+        {
+            LastNotified = Readings.Keys.Max();
 
             var lastEntry = Readings[LastNotified];
             if (lastEntry.Length != 3) throw new Exception("Wrong count of Entries");
             BoilerWarm = (ClampPercent(lastEntry[0]) + ClampPercent(lastEntry[1]) + ClampPercent(lastEntry[2])) / 3;
             BoilerTime = BoilerWarm * 45;
+
         }
 
         private static double ClampPercent(double value)
@@ -29,18 +35,38 @@
 
 
         public static readonly BoilerMeta Default = new(
-        new Dictionary<DateTime, double[]>()
+            new Dictionary<DateTime, double[]>()
+            {
+                {DateTime.UtcNow.AddMinutes(-9), new double[]{20,20,20 } },
+                {DateTime.UtcNow.AddMinutes(-8), new double[]{25,20,20 } },
+                {DateTime.UtcNow.AddMinutes(-7), new double[]{30,20,20 } },
+                {DateTime.UtcNow.AddMinutes(-6), new double[]{35,25,20 } },
+                {DateTime.UtcNow.AddMinutes(-5), new double[]{40,30,20 } },
+                {DateTime.UtcNow.AddMinutes(-4), new double[]{45,30,20 } },
+                {DateTime.UtcNow.AddMinutes(-3), new double[]{50,35,20 } },
+                {DateTime.UtcNow.AddMinutes(-2), new double[]{50,35,25 } },
+                {DateTime.UtcNow.AddMinutes(-1), new double[]{50,40,25 } },
+                {DateTime.UtcNow.AddMinutes(-0), new double[]{50,40,30 } },
+            });
+
+        private static readonly Random _rnd = new();
+        private static readonly Thread _pureChaos = GetThread();
+
+        private static Thread GetThread()
         {
-            {DateTime.UtcNow.AddMinutes(-9), new double[]{20,20,20 } },
-            {DateTime.UtcNow.AddMinutes(-8), new double[]{25,20,20 } },
-            {DateTime.UtcNow.AddMinutes(-7), new double[]{30,20,20 } },
-            {DateTime.UtcNow.AddMinutes(-6), new double[]{35,25,20 } },
-            {DateTime.UtcNow.AddMinutes(-5), new double[]{40,30,20 } },
-            {DateTime.UtcNow.AddMinutes(-4), new double[]{45,30,20 } },
-            {DateTime.UtcNow.AddMinutes(-3), new double[]{50,35,20 } },
-            {DateTime.UtcNow.AddMinutes(-2), new double[]{50,35,25 } },
-            {DateTime.UtcNow.AddMinutes(-1), new double[]{50,40,25 } },
-            {DateTime.UtcNow.AddMinutes(-0), new double[]{50,40,30 } },
-        });
+            var t = new Thread(delegate ()
+            {
+                while (true)
+                {
+                    Thread.Sleep(3000);
+                    var lastValues = Default.Readings[Default.Readings.Keys.Max()];
+                    Default.Readings.Add(DateTime.UtcNow, new double[] { lastValues[0] + _rnd.NextDouble() * 3 - 2, lastValues[1] + _rnd.NextDouble() * 3 - 2, lastValues[2] + _rnd.NextDouble() * 3 - 2 });
+                    Default.Update();
+                }
+            })
+            { IsBackground = true };
+            t.Start();
+            return t;
+        }
     }
 }
--- BoilerManager/wwwroot/js/site.js
+++ BoilerManager/wwwroot/js/site.js
@@ -15,9 +15,14 @@ function drawCylinder(heightCylinder, stepPx, cylinderWarm) {
     $('#bottom')[0].style.marginTop = heightCylinder - 100 + 'px';
 }
 function setMetrics(cylinderWarm, cylinderTime) {
-    $('#boilerPercent')[0].innerText = (cylinderWarm*100).toFixed(2);
+    $('#boilerPercent')[0].innerText = (cylinderWarm * 100).toFixed(2);
     $('#boilerTime')[0].innerText = (cylinderTime).toFixed(0);
 }
+function dateToLocal(date) {
+    var d = date ? new Date(date) : new Date();
+    return d.toLocaleString();
+}
+var globalChart;
 function update(gHeightPx, gStepPx) {
     fetch("/api/BoilerData")
         .then(response => {
@@ -29,12 +34,45 @@ function update(gHeightPx, gStepPx) {
                         var cylinderTime = json.boilerTime;
                         drawCylinder(gHeightPx, gStepPx, cylinderWarm);
                         setMetrics(cylinderWarm, cylinderTime);
-                        return Promise.resolve(`Обновлено: клиент: ${new Date().toLocaleString()}, сервер: ${new Date(json.lastNotified).toLocaleString()}`);
+                        //---------------
+                        var data = {
+                            labels: [],
+                            datasets: [{
+                                label: 'Top reading',
+                                backgroundColor: 'rgb(255, 99, 132)',
+                                borderColor: 'rgb(255, 99, 132)',
+                                data: [],
+                            }, {
+                                label: 'Mid reading',
+                                backgroundColor: 'rgb(99, 255, 132)',
+                                borderColor: 'rgb(132, 255, 99)',
+                                data: [],
+                            }, {
+                                label: 'Bottom reading',
+                                backgroundColor: 'rgb(132, 99, 255)',
+                                borderColor: 'rgb(99, 255, 132)',
+                                data: [],
+                            }]
+                        };
+                        for (var k of Object.keys(json.readings)) {
+                            data.labels.push(dateToLocal(k));
+                            values = json.readings[k];
+                            for (var i = 0; i < 3; i++) {
+                                data.datasets[i].data.push(values[i]);
+                            }
+                        }
+                        if (globalChart) globalChart.destroy();
+                        globalChart = new Chart($('#myChart'), { type: 'line', data: data, options: { animation:false } });
+
+                        //---------------
+                        setTimeout(() => update(gHeightPx, gStepPx), 1000);
+                        //---------------
+                        return Promise.resolve(`Обновлено: клиент: ${dateToLocal()}, сервер: ${dateToLocal(json.lastNotified)}`);
                     });
             } else {
                 statusPromise = response.text()
                     .then(text => {
-                        return Promise.resolve(`Обновлено: клиент: ${new Date().toLocaleString()}, ошибка HTTP ${response.status} ${response.statusText}: [${text}]`);
+                        return Promise.resolve(`Обновлено: клиент: ${dateToLocal()}, ошибка HTTP ${response.status} ${response.statusText}: [${text}]`);
                     });
             }
             return statusPromise;
