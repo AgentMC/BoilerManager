a87da0de2e035dc027aa79c5507a8a975c0d9dc3
AgentMC <hackdesigner2010@hotmail.com>
2023-03-28 00:29:00 +0100

    Persistance.

--- BoilerManager/Model/BoilerMeta.cs
+++ BoilerManager/Model/BoilerMeta.cs
@@ -2,11 +2,41 @@
 {
     public class BoilerMeta : BoilerMetaBase
     {
-        const double FullWarm = 45.0, DefCold = 25, TimeToCool = 45;
         public const int MAX_ENTRIES = 7*24*60;
+        const double FullWarm = 45.0, DefCold = 25, TimeToCool = 45;
+
+        private static readonly string PersistanceFile = Path.Combine(Environment.ExpandEnvironmentVariables("%TEMP%"), "BoilerManagerCache.dat");
 
         private readonly List<(DateTime timestamp, double[] values)> Readings = new();
         private readonly ReaderWriterLockSlim block = new();
+        private readonly AutoResetEvent goWrite = new(false);
+        
+        public BoilerMeta() 
+        {
+            var c = System.Globalization.CultureInfo.InvariantCulture;
+            if (File.Exists(PersistanceFile))
+            {
+                using var reader = new StreamReader(PersistanceFile, System.Text.Encoding.UTF8);
+                var entry = reader.ReadLine()?.Split('|');
+                if(entry?.Length == 3)
+                {
+                    BoilerTime = double.Parse(entry[0], c);
+                    BoilerWarm = double.Parse(entry[1], c);
+                    LastNotified = DateTime.Parse(entry[2]);
+                    while (!reader.EndOfStream)
+                    {
+                        entry = reader.ReadLine()?.Split('|');
+                        if(entry?.Length == 4)
+                        {
+                            Readings.Add((DateTime.Parse(entry[0]), new[] { double.Parse(entry[1], c), double.Parse(entry[2], c), double.Parse(entry[3], c) }));
+                        }
+                    }
+                }
+            }
+
+            var persister = new Thread(PersisterThread) { IsBackground = true, Name = nameof(PersisterThread), CurrentCulture = c };
+            persister.Start();
+        }
 
         public void Add(double[] values)
         {
@@ -24,6 +54,7 @@
                 UpdateStats(timestamp, values);
             }
             block.ExitWriteLock();
+            goWrite.Set();
         }
 
         private void UpdateStats(DateTime timestamp, double[] values)
@@ -45,8 +76,8 @@
             block.EnterReadLock();
             {
                 r = new BoilerMetaResponse(LastNotified, BoilerWarm, BoilerTime);
-                if (count == -1) count = Readings.Count;
-                for (int i = Math.Max(Math.Max(Readings.Count - count, 0), Readings.Count - 1); i > 0 && i < Readings.Count; i++)
+                if (count < 0 || count > Readings.Count) count = Readings.Count;
+                for (int i = Readings.Count - count; i >= 0 && i < Readings.Count; i++)
                 {
                     var (timestamp, values) = Readings[i];
                     r.Readings[timestamp] = values;
@@ -57,5 +88,23 @@
         }
 
         public static readonly BoilerMeta Default = new();
+
+        private void PersisterThread()
+        {
+            while (true)
+            {
+                goWrite.WaitOne();
+                block.EnterReadLock();
+                {
+                    using var writer = new StreamWriter(PersistanceFile, System.Text.Encoding.UTF8, new FileStreamOptions { Mode = FileMode.Create, Access = FileAccess.Write, Share = FileShare.ReadWrite | FileShare.Delete });
+                    writer.WriteLine($"{BoilerTime}|{BoilerWarm}|{LastNotified:O}");
+                    foreach (var (timestamp, values) in Readings)
+                    {
+                        writer.WriteLine($"{timestamp:O}|{values[0]}|{values[1]}|{values[2]}");
+                    }
+                }
+                block.ExitReadLock();
+            }
+        }
     }
 }
